57b4d05952b157f1fc006704e2085074f589fc51
Ankush Singh <104047775+NeverGiveUp-086@users.noreply.github.com>
2022-12-25 23:28:58 +0530

    Create Reverse a linked list.cpp
    Given a linked list of N nodes. The task is to reverse this list.
    
    Example 1:
    
    Input:
    LinkedList: 1->2->3->4->5->6
    Output: 6 5 4 3 2 1
    Explanation: After reversing the list, 
    elements are 6->5->4->3->2->1.
    Example 2:
    
    Input:
    LinkedList: 2->7->8->9->10
    Output: 10 9 8 7 2
    Explanation: After reversing the list,
    elements are 10->9->8->7->2.
    Your Task:
    The task is to complete the function reverseList() with head reference as the only argument and should return new head after reversing the list.
    
    Expected Time Complexity: O(N).
    Expected Auxiliary Space: O(1).
    
    Constraints:
    1 <= N <= 104
    
    Link
    https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1

---
+++ Linked List/Reverse a linked list.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *head)
+    {
+        // code here
+        // return head of reversed list
+        Node* prev=NULL;
+        Node* curr=head;
+        Node* nxt=NULL;
+        
+        while(curr!=NULL){
+            nxt=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+        }
+        return prev;
+    }
+    
+};
