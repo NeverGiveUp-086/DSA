ed6af2d455dd8b2ef282a0b207813e94f187e5ec
Ankush Singh <104047775+NeverGiveUp-086@users.noreply.github.com>
2023-02-19 17:30:52 +0530

    Create Floor in BST.cpp
    You are given a BST(Binary Search Tree) with n number of nodes and value x. your task is to find the greatest value node of the BST which is smaller than or equal to x.
    Note: when x is smaller than the smallest node of BST then returns -1.
    
    Example:
    
    Input:
    n = 7               2
                         \
                          81
                        /     \
                     42       87
                       \       \
                        66      90
                       /
                     45
    x = 87
    Output:
    87
    Explanation:
    87 is present in tree so floor will be 87.
    Example 2:
    
    Input:
    n = 4                     6
                               \
                                8
                              /   \
                            7       9
    x = 11
    Output:
    9
    Your Task:
    You don't need to read input or print anything. Complete the function floor() which takes the integer n and BST and integer x returns the floor value.
    
    Constraint:
    1 <= n <= 105
    
    Expected Time Complexity: O(n)
    Expected Space Complexity: O(1)

---
+++ BST/Floor in BST.cpp
@@ -0,0 +1,28 @@
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+int floor(Node *root, int x);
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
