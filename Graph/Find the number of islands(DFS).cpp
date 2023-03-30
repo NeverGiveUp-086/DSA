8f3d899479cb4bf101f0a4431505d157602779e9
Ankush Singh <104047775+NeverGiveUp-086@users.noreply.github.com>
2023-03-30 23:46:08 +0530

    Create Find the number of islands(DFS).cpp

---
+++ Graph/Find the number of islands(DFS).cpp
@@ -0,0 +1,46 @@
+
+class Solution {
+  private:
+    // Function to find the number of islands.
+    void  dfs(int & i, int &j, vector<vector<int>>&vis,vector<vector<char>>&grid){
+        vis[i][j]=1;
+         int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        while(!q.empty()){
+            int ni=q.front().first;
+            int nj=q.front().second;
+            q.pop();
+          for(int x=-1;x<=1;x++){
+              for(int y=-1;y<=1;y++){
+                  int dx=x+ni;
+                  int dy=y+nj;
+                  if(dx>=0 && dy>=0 && dx<n && dy<m && vis[dx][dy]==0 && grid[dx][dy]=='1'){
+                    
+                   dfs(dx,dy,vis,grid); //dfs
+                  }
+              }
+          }
+        }
+    }
+    public:
+    int numIslands(vector<vector<char>>& grid) {
+        // Code here
+        int ans=0;
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0 && grid[i][j]=='1'){
+                   dfs(i,j,vis,grid);
+                   ans+=1;
+                }
+            }
+        }
+        return ans;
+    }
+
+};
+
