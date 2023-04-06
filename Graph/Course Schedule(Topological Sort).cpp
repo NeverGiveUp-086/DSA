a20afe05d08b955e140876ffbe2014a9c9b5246f
Ankush Singh <104047775+NeverGiveUp-086@users.noreply.github.com>
2023-04-06 20:52:15 +0530

    Create Course Schedule(Topological Sort).cpp

---
+++ Graph/Course Schedule(Topological Sort).cpp
@@ -0,0 +1,50 @@
+//User function Template for C++
+
+class Solution
+{
+  public:
+    vector<int>findOrder(int n, int m, vector<vector<int>> arr) 
+    {
+        //code here
+        vector<int>indeg(n,0);
+        vector<int>ans;
+        vector<int>adj[n];
+           
+        for(int i=0;i<m;i++){
+            adj[arr[i][1]].push_back(arr[i][0]);
+        }
+        
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            for(auto x:adj[i])
+            indeg[x]++;
+        }
+        
+        for(int i=0;i<n;i++){
+            if(indeg[i]==0)
+            q.push(i);
+        }
+        
+        while(q.size()>0){
+            int u=q.front();
+            q.pop();
+            ans.push_back(u);
+            
+            for(auto k:adj[u]){
+                indeg[k]--;
+                if(indeg[k]==0)
+                q.push(k);
+            }
+        }
+        if(ans.size()==n)
+            return ans;
+        
+        
+        return {};
+        
+    }
+};
+
+
+Expected Time Complexity: O(n+m).
+Expected Auxiliary Space: O(n+m).
