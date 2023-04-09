8ae20fdb93232aec6aa445942e628532f6601c22
अंकुश सिंह <104047775+NeverGiveUp-086@users.noreply.github.com>
2023-04-10 01:38:33 +0530

    Create Solution.cpp

---
+++ Greedy/Solution.cpp
@@ -0,0 +1,17 @@
+   1. N meetings in one room //gfg
+       https://practice.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1
+
+   2. Maximum Meetings in One Room //gfg
+     https://practice.geeksforgeeks.org/problems/maximum-meetings-in-one-room/1
+  
+   3. Shop in Candy Store //gfg
+      https://practice.geeksforgeeks.org/problems/shop-in-candy-store1145/1
+
+  4. Check if it is possible to survive on Island //gfg
+     https://practice.geeksforgeeks.org/problems/check-if-it-is-possible-to-survive-on-island4922/1
+
+  5.  Reverse words in a given string //gfg
+     https://practice.geeksforgeeks.org/problems/reverse-words-in-a-given-string5459/1
+
+  6. 
+     
