b2dfbf5d490ca302888c417ee6e2cf3cbead8b55
Ankush Singh <104047775+NeverGiveUp-086@users.noreply.github.com>
2023-05-13 17:48:59 +0530

    Create 11  Longest Common Substring.cpp
    https://practice.geeksforgeeks.org/problems/longest-common-substring1452/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article

---
+++ Dynamic Programming/11  Longest Common Substring.cpp
@@ -0,0 +1,28 @@
+class Solution{
+    public:
+    
+    int longestCommonSubstr (string s1, string s2, int n, int m)
+    {
+        // your code here
+        int dp[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                if(i==0 || j==0)
+                dp[i][j]=0;
+            }
+        }
+         
+        int ans=0;
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                    ans=max(ans,dp[i][j]);
+                }
+                else
+                dp[i][j]=0;
+            }
+        }
+        return ans;
+    }
+};
